d1dc9399dfa5b3be9a633e6ebd237291a7742486
cyblster <kiuspensky@gmail.com>
2023-05-21 11:52:30 +0500

    bug fixes

--- app/devices/__init__.py
+++ app/devices/__init__.py
@@ -5,6 +5,7 @@ from app.clouds.tcl_cloud import TclCloud
 from .fan import Standing2Fan
 from .vacuum import Lite2Vacuum
 from .humidifier import Humidifier2
+from .humidifier_sensor import Humidifier2Sensor
 from .intercom import DomRuApi
 from .ac import TclAC
 
@@ -13,6 +14,7 @@ mi_cloud = MiCloud(config.MI_CLOUD_USERNAME, config.MI_CLOUD_PASSWORD, 'ru')
 fan = Standing2Fan(mi_cloud)
 vacuum = Lite2Vacuum(mi_cloud)
 humidifier = Humidifier2(mi_cloud)
+humidifier_sensor = Humidifier2Sensor(mi_cloud)
 
 # TCL
 # tcl_cloud = TclCloud(config.TCL_CLOUD_USERNAME, config.TCL_CLOUD_PASSWORD, 'ru')
@@ -25,6 +27,7 @@ devices = {
     'dmaker.fan.p18': fan,
     'ijai.vacuum.v10': vacuum,
     'deerma.humidifier.jsq2w': humidifier,
+    'deerma.humidifier.jsq2w.sensor': humidifier_sensor,
     'domru.intercom.355': intercom,
     # 'tcl.ac.355': ac
 }
